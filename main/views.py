25392cb93be91ccc0e9745c14b5b091c6fadbecf
mac <mac@MacBook-Air-alkhas-b-mac.local>
2019-09-01 21:47:57 +0300

    edit

--- main/views.py
+++ main/views.py
@@ -11,11 +11,11 @@ def articles_list(request):
     }
     return render(request, "articles_list.html", context)
 
-    def article_details(request, article_id):
+def article_details(request, article_id):
     context = { "article" : Article.objects.get(id=article_id)}
     return render(request, 'article_details.html', context)
 
-    def create_article(request):
+def create_article(request):
 	form = ArticleForm()
 	if request.method == "POST":
 		form = ArticleForm(request.POST)
@@ -28,3 +28,17 @@ def articles_list(request):
 	context = {"form" : form}
 
 	return render(request, "create_article.html", context)
+
+def edit_article(request, article_id):
+	article = Article.objects.get(id=article_id)
+
+	form = ArticleForm(instance=article)
+	if request.method == "POST":
+		form = ArticleForm(request.POST, instance=article)
+
+		if form.is_valid():
+		    form.save()
+			return redirect('article-details', article_id)
+
+	context = {"form":form, "article":article}
+	return render(request, 'edit_article.html', context)
